372610b290b910c0a01383906a1de1854eff4f32
Seth McGinnis <mcginnis@ucar.edu>
2019-04-22 19:32:22 -0600

    Changed output formatting

--- which-missing.R
+++ which-missing.R
@@ -1,21 +1,78 @@
 args<-commandArgs(TRUE)
 library(ncdf4)
-
-#infile = "prec.hist.GFDL-ESM2M.WRF.day.NAM-44i.raw.nc"
+library(PCICt)
 
 infile  = args[1]
+## For testing:
+#infile = "~/DATA/cordex/data/raw/NAM-44/day/WRF/MPI-ESM-LR/rcp85/tasmin/tasmin.rcp85.MPI-ESM-LR.WRF.day.NAM-44.raw.nc"
 
-varname <- unlist(strsplit(basename(infile), ".", fixed=TRUE))[1]
 
-fin<-nc_open(infile)
+# Pass further arguments to control output:
+# quiet: don't print filename
+# range: print ranges instead of enumerating values
+# count, date, index: print count/date/index of missing timesteps 
+
+fname <- basename(infile)
+
+varname <- unlist(strsplit(fname, ".", fixed=TRUE))[1]
+
+nc <- nc_open(infile)
 
 bad <- c()
 
-for(i in 1:fin$dim$time$len){
-#  cat(i," ")
-  slab <- ncvar_get(fin, varname, start=c(1,1,i), count=c(-1,-1,1))
+## Looping over timesteps is MUCH faster than reading the whole thing in
+for(i in 1:nc$dim$time$len){
+  slab <- ncvar_get(nc, varname, start=c(1,1,i), count=c(-1,-1,1))
   if(all(is.na(slab))) { bad <- c(bad,i) }
 }
 
-cat(infile, "\t", length(bad),"\n")
-cat(bad, "\n")
+
+
+## Condense vector into runs of sequential values
+
+headtail <- function(x){
+  if(length(x) > 1) {c(head(x,1), tail(x,1))} else {x}
+}
+
+condense <- function(x){
+  runs <- split(x, c(0, cumsum(diff(x) > 1)))
+  names(runs) <- NULL
+  lapply(runs, headtail)
+}
+
+
+runs <- condense(bad)
+
+
+
+## Print count of missing timesteps
+
+cat(fname, "\t", length(bad),"\n")
+
+
+
+## Print indices of missing timesteps
+indexes <- sapply(runs, paste, collapse="-")
+
+cat(paste(indexes, collapse=", "), "\n")
+
+
+## Print dates of missing timesteps
+calendar <- ncatt_get(nc, "time", "calendar")$value
+ustring  <- strsplit(ncatt_get(nc, "time", "units")$value, " ")[[1]]
+tunits   <- ustring[1]
+epoch    <- ustring[3]
+if(length(ustring) > 3){ epoch <- paste(epoch, ustring[4]) }
+   
+timetodate <- function(x){
+  conversions <- c(days=24*60*60, hours=60*60, minutes=60, seconds=1)
+  if(! tunits %in% names(conversions)){
+    stop("Conversion factor from ", tunits, " to seconds for PCICt not known.")
+  } 
+  as.character(as.PCICt(x*conversions[tunits], cal=calendar, origin=epoch))
+}
+
+dates <- sapply(rapply(runs, timetodate, how="replace"), paste, collapse=" to ")
+
+cat(paste(dates, collapse="; "), "\n")
+
